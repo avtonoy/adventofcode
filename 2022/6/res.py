bc8ad2b534a84b00dfd14c9f54f23ed5cfd9a74b
Sergej Geist <sergej.geist@oth-regensburg.de>
2023-11-14 17:57:45 +0100

    mod

--- 2022/6/res.py
+++ 2022/6/res.py
@@ -13,25 +13,19 @@ text=text[0]
 # Wie viel aufeinander folgende Buchstaben dürfen nicht gleich sein. 
 Anzahlverschieden=4
 Vergleichstring=''
-Zaehlerungleich=0
+
 
 for index,Buchstabe in enumerate(text): 
     Vergleichstring+=Buchstabe # hinzufügen eines weitern Buchstabens 
     # Es werden immer nur eine bestimmt Anzahl von Buchstaben miteinandern vergliehcen  
-    Gleich = False
-    if len(Vergleichstring)>Anzahlverschieden: 
+    LngVergleich=len(Vergleichstring) 
+    
+    if LngVergleich>Anzahlverschieden: 
         Vergleichstring=Vergleichstring[1:]
+        LngVergleich-=1
     
     # der letzt hinzugefügte Buchstaben wird mit allen vorangegangen Buchstaben verglichen. 
-    for Vergleichsindex in range(len(Vergleichstring)-1): 
-        Vergleichsbuchstabe=Vergleichstring[Vergleichsindex]
-        if Vergleichsbuchstabe==Buchstabe:
-            Gleich=True 
-            break
-    
-    if Gleich != True: 
-        Zaehlerungleich+=1
-        if Zaehlerungleich ==4:
-            print(index)
-    else: 
-        Zaehlerungleich-=1
+    for AnzahluebringeBuchstaben in range(LngVergleich):
+        for Vergleichsindex in reversed(range(len(Vergleichstring)-AnzahluebringeBuchstaben-1)): 
+            if Vergleichstring[AnzahluebringeBuchstaben]!=Vergleichstring[Vergleichsindex]:
+               print('Ungleich') 
