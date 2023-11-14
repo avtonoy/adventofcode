7bb784b465055a5c4fee6772d79a59291709d726
Sergej Geist <sergej.geist@oth-regensburg.de>
2023-11-14 16:40:14 +0100

    Tag 5 Aufgbae 1 erfolgreich gelöst

--- 2022/5/res.py
+++ 2022/5/res.py
@@ -1,5 +1,5 @@
 
-with open('inputtest', 'r') as f:
+with open('input', 'r') as f:
     text=f.readlines() 
     f.close()
     
@@ -38,7 +38,38 @@ Aufbau={}
 for index in range(len(Vektoraufbaustr)): 
     Aufbau.update({int(Vektoraufbaustr[index]):[]}) 
 
+# Befüllung der Aufbau Bibliothek 
 for line in reversed(header):
-    Reihe=line.split()
+    
+    # Schreibe ein |X| rein um Lückenzweifelsfrei zu markieren
+    Reihe=line.replace('    ',' |X| ').split()
+
     for index,Reiheneintrag in enumerate(Reihe): 
-        Aufbau[index+1].append(Reiheneintrag)
+        if Reiheneintrag != '|X|':
+            Aufbau[index+1].append(Reiheneintrag)
+
+
+
+for Anweisungstext in text: 
+        Anweisung=Anweisungstext.split()
+        Anzahl=int(Anweisung[1])
+        Von=int(Anweisung[3])
+        Nach=int(Anweisung[5])
+        
+        for aktion in range(Anzahl): 
+            Aufbau[Nach].append(Aufbau[Von].pop())
+
+stringout=''
+keys=list(Aufbau.keys())
+
+# Ermittle die oben liegenden Elemente
+for key in keys: 
+    stringout+=Aufbau[key][-1]
+
+# Entferne die Klammern um die Bustaben 
+stringout=stringout.replace('[', '')
+stringout=stringout.replace(']', '')
+
+
+print(Aufbau)
+print(stringout)
