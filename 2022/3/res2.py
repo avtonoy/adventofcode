74cddc8aae395d1f58e8b5ae9aa7d56b3ee41029
Sergej Geist <sergej.geist@oth-regensburg.de>
2023-11-14 10:50:35 +0100

    Tag3 abgeschlossen

--- 2022/3/res2.py
+++ 2022/3/res2.py
@@ -26,14 +26,16 @@ with open('input', 'r') as f:
 
 Compartment1=[]
 Compartment2=[]
+Compartment3=[]
 GleichItem=[]
 
 
 
-for Zeile in text:
+for Indextext in range(0,len(text),3):
     # Zerlege die Kompartments nach codierung genau in der hälfte 
-    Compartment1.append(Zeile[: len(Zeile)//2])
-    Compartment2.append(Zeile[len(Zeile)//2 :])
+    Compartment1.append(text[Indextext][:-1])
+    Compartment2.append(text[Indextext+1][:-1])
+    Compartment3.append(text[Indextext+2][:-1])
     
     # Vergleiche die Compartment und schreibe buchstaben 
     # die in beiden compartsment vorkommen nieder
@@ -42,16 +44,17 @@ for Zeile in text:
     
     for Zeichen1 in Compartment1[-1]:
         for Zeichen2 in Compartment2[-1]:
-            if Zeichen2 == Zeichen1:
-                LogischEintrag=True # Es wird davon ausgegangen dass der Eintrag gemacht wird
-                # Falls der Eintrag schon vorhanden ist wird dieser nicht getätigt 
-                for ZeicheninGleichItem in EintraginGleichItem: 
-                    if Zeichen1==ZeicheninGleichItem:
-                        LogischEintrag=False
-                        break
-                # Fall durch einen Doppelt Eintrag dieser nicht gehindert wird so wird dieser getätigt
-                if LogischEintrag:
-                    EintraginGleichItem=EintraginGleichItem+Zeichen1
+            for Zeichen3 in Compartment3[-1]:
+                if Zeichen2 == Zeichen1==Zeichen3:
+                    LogischEintrag=True # Es wird davon ausgegangen dass der Eintrag gemacht wird
+                    # Falls der Eintrag schon vorhanden ist wird dieser nicht getätigt 
+                    for ZeicheninGleichItem in EintraginGleichItem: 
+                        if Zeichen1==ZeicheninGleichItem:
+                                LogischEintrag=False
+                                break
+                    # Fall durch einen Doppelt Eintrag dieser nicht gehindert wird so wird dieser getätigt
+                    if LogischEintrag:
+                        EintraginGleichItem=EintraginGleichItem+Zeichen1
     
     GleichItem.append(EintraginGleichItem)
     
